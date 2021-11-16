aeced564d9ab84017c74945125ebd0e9223c0c83
Aidar <achakiev2003@gmail.com>
2021-11-16 20:38:07 +0600

    push init

---
+++ app/src/main/java/pro/aidar/mealsapp/firebase/MyFirebaseMessagingService.kt
@@ -0,0 +1,12 @@
+package pro.aidar.mealsapp.firebase
+
+import androidx.core.app.NotificationCompat
+import com.google.firebase.messaging.FirebaseMessagingService
+
+const val channelId = "notification_center"
+const val channelName = "pro.aidar.mealsapp"
+class MyFirebaseMessagingService: FirebaseMessagingService() {
+
+
+
+}
